5231bcfdff15cfaef5d1a9e0cca9f662e170663b
Prateek07 <prtk.sultania@gmail.com>
2026-07-22 17:54:20 +0530

    Delete infra/modules/EventGridSubscription directory

--- infra/modules/EventGridSubscription/output.tf
+++
@@ -1,4 +0,0 @@
-output "eventGridSubscription" {
-  description = "Map of Event Grid system topic event subscriptions."
-  value       = azurerm_eventgrid_system_topic_event_subscription.eventGridSubscription
-}
